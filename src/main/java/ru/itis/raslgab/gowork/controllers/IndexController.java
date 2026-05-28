3605d87e692d28d32089fb2c9fad570a42cd7470
Rasil Gabdelganiev <gabdelganievrasil@gmail.com>
2026-05-28 20:14:57 +0300

    feat: add index page

--- src/main/java/ru/itis/raslgab/gowork/controllers/IndexController.java
+++ src/main/java/ru/itis/raslgab/gowork/controllers/IndexController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class IndexController {
-
     @GetMapping
     public String index() {
         return "index";
